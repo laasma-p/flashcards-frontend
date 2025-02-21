8252bc32b885a0085e411ac430fe6ca9a3926b8f
Lasma Poksane <laasmapoksane@gmail.com>
2025-02-21 17:10:39 +0100

    Render AuthForm component for registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router";
 import Hero from "./components/Hero";
+import AuthForm from "./containers/AuthForm";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Hero />} />
+      <Route path="/register" element={<AuthForm />} />
     </Routes>
   );
 }
